974635bd6b77d96430570ac2746e4359b5b6f70e
Jared Kofron <jared.kofron@gmail.com>
2012-07-13 11:16:46 -0700

    first try at a sweepline module

---
+++ src/instruments/sweepline.erl
@@ -0,0 +1,42 @@
+% sweepline.erl
+% A frontend for the sweepline utility.
+-module(sweepline).
+-behavior(gen_os_cmd).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% gen_os_cmd callbacks %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+-export([init/1,start_link/1]).
+-export([base_cmd/0,process_args/2]).
+
+%%%%%%%%%%%%%%%%%%%%%%
+%%% internal state %%%
+%%%%%%%%%%%%%%%%%%%%%%
+-record(state, {}).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% callback definitions %%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+start_link(ID) ->
+    gen_os_cmd:start_link(?MODULE,ID).
+
+init(_Args) ->
+    {ok, #state{}}.
+
+base_cmd() ->
+    "/home/grybka/sweepline/sweepline".
+
+process_args(Args, _StateData) ->
+    proplist_to_args(Args, []).
+
+proplist_to_args([],Acc) ->
+    lists:reverse(Acc);
+proplist_to_args([{points, P}|T], Acc) ->
+    Str = lists:concat(["-f ", P]),
+    proplist_to_args(T,[Str | Acc]);
+proplist_to_args([{events, N}|T], Acc) ->
+    Str = lists:concat(["-n ", N]),
+    proplist_to_args(T,[Str | Acc]);
+proplist_to_args([{input, I}|T], Acc) ->
+    Str = lists:concat(["-i",I]),
+    proplist_to_args(T,[Str | Acc]).
