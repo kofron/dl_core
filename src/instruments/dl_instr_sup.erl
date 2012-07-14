7d341c080a6e765aec78bc14ca821cabb3aba989
Jared Kofron <jared.kofron@gmail.com>
2012-07-14 16:21:46 -0700

    start the eprologix supervisor also

--- src/instruments/dl_instr_sup.erl
+++ src/instruments/dl_instr_sup.erl
@@ -24,5 +24,6 @@ start_link() ->
 %% ===================================================================
 
 init([]) ->
-    {ok, { {one_for_one, 5, 10}, []} }.
+    EproSup = ?CHILD(eprologix_sup, supervisor),
+    {ok, { {one_for_one, 5, 10}, [EproSup]} }.
 
